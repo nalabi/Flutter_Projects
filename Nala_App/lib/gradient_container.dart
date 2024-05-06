3341cc0e03415e2554de9045032828f890426fa9
nalabi <bongimusa.nala@gmail.com>
2024-05-06 15:13:59 +0200

    hard push

--- Nala_App/lib/gradient_container.dart
+++ Nala_App/lib/gradient_container.dart
@@ -1,28 +1,55 @@
-import 'package:first_app/styled_text.dart';
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
+  
 
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: const [
-            Colors.deepOrangeAccent,
-            Color.fromRGBO(50, 231, 4, 0.87),
-          ],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText(),
+      child: Center(
+        child: DiceRoller(),
       ),
     );
   }
 }
+
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText("Sample styled Text"),
+//       ),
+//     );
+//   }
+// }
--- Nala_App/lib/main.dart
+++ Nala_App/lib/main.dart
@@ -4,9 +4,10 @@ import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-   const MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer( Color.fromARGB(221, 111, 17, 134),
+            Color.fromARGB(137, 172, 207, 43)),
       ),
     ),
   );
