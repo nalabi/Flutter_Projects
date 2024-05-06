b818663c12738d8f1b731ed26689424a9f87032b
nalabi <bongimusa.nala@gmail.com>
2024-05-06 14:19:36 +0200

    454

---
+++ Nala_App/lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:first_app/styled_text.dart';
+import 'package:flutter/material.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: const [
+            Colors.deepOrangeAccent,
+            Color.fromRGBO(50, 231, 4, 0.87),
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
---
+++ Nala_App/lib/main.dart
@@ -0,0 +1,13 @@
+//import 'package:Nala_App/gradient_container';
+import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
+
+void main() {
+  runApp(
+   const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
